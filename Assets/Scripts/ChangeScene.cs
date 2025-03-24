69c2f03342424be48ba9e720db59f0ac32b73a2d
Koonfu24 <nontarit.arun@bumail.net>
2025-03-24 15:03:12 +0700

    Credit

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,19 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-   public void Start()
+   public void StartGame()
    {
       SceneManager.LoadScene("RollerCandy");
    }
 
+   public void Credit()
+   {
+      SceneManager.LoadScene("CreditScene.unity");
+   }
+
    public void Back()
    {
-      SceneManager.LoadScene("MainMenu");
+      SceneManager.LoadScene("MainMenu.unity");
    }
-   
+ 
    public void Quit()
    {
       Application.Quit();
    }
-  
+ 
 }
---
+++ Assets/Scripts/ChangeToCreditScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeToCreditScene : MonoBehaviour
+{
+    public void Credit1()
+    {
+       SceneManager.LoadScene("CreditScene"); 
+    }
+}
