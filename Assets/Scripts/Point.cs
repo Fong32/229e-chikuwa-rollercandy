9c00b890a32a87891e0b7633151d65a8c1a14c7b
Koonfu24 <nontarit.arun@bumail.net>
2025-03-25 16:46:12 +0700

    .

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -5,12 +5,14 @@ public class Point : MonoBehaviour
   public CollectPoint collectPoint;
   public CollectPoint collectPoint2;
   public CollectPoint collectPoint3;
+  public AudioSource sound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
+            sound.Play();
             
         }
         collectPoint.AddPoint();
