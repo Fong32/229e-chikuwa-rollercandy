59b195c7b84aee7c0f151e32fa0bafde3b5b9a70
Koonfu24 <nontarit.arun@bumail.net>
2025-03-25 16:11:21 +0700

    update music

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{ 
+    private static DontDestroy instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
